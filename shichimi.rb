735777ce4f69b82119fa33cc3d2c1c459cdd757c
Tomoharu Ito <treeoflife1103@gmail.com>
2017-12-21 01:10:18 +0900

    Devekopmet of SHICHIMI

--- shichimi.rb
+++ shichimi.rb
@@ -4,6 +4,15 @@ require 'uri'
 require 'json'
 require 'pp'
 
-keys = File.open("data.json")
-client = Twitter::REST::Client.new(keys)
-pp client
+h = File.open("data.json"){|f| JSON.load(f)}
+keys = h[0]
+client_rest = Twitter::REST::Client.new(keys)
+client_streaming = Twitter::Streaming::Client.new(keys)
+
+uri_nagano = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200010') # 長野
+uri_matsumoto = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200020') # 松本
+uri_iida = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200030') # 飯田
+json = Net::HTTP.get(uri_matsumoto)
+result = JSON.parse(json)
+pp result['description']['text']
+
