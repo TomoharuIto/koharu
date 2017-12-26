bc13e36dcc1a1c14d44d9c9405db9f8678d595dd
Tomoharu Ito <treeoflife1103@gmail.com>
2017-12-27 00:57:59 +0900

    Add new file

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source :rubygems
 
 gem 'twitter'
-gem 'net/http'
-gem 'uri'
 gem 'json'
 gem 'date'
 gem 'clockwork'
 gem 'sinatra'
+gem 'dotenv'
--- shichimi.rb
+++ shichimi.rb
@@ -5,12 +5,17 @@ require 'json'
 require 'date'
 require 'clockwork'
 require 'pp'
+require 'dotenv'
 
-h = File.open("data.json"){|f| JSON.load(f)}
-keys = h[0]
-client_rest = Twitter::REST::Client.new(keys)
+Dotenv.load
+client_rest = Twitter::REST::Client.new(
+  consumer_key: ENV['CONSUMER_KEY'],
+  consumer_secret: ENV['CONSUMER_SECRET'],
+  access_token: ENV['ACCESS_TOKEN'],
+  access_token_secret: ENV['ACCESS_TOKEN_SECRET']
+)
+
 # client_streaming = Twitter::Streaming::Client.new(keys)
-
 uri_nagano = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200010')
 uri_matsumoto = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200020')
 uri_iida = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200030')
@@ -21,11 +26,11 @@ date_time = DateTime.parse(public_time)
 suffix = %w(お を の もふ よ ぽ と)
 announcement_time = date_time.strftime("%m月%d日 %H時%M分 発表の予報です#{suffix.sample}。\n\n")
 weather = result['description']['text']
-weather_forecast = (announcement_time << weather).scan(/.{1,139}。$/m).reverse
-pp weather_forecast
-# include Clockwork
-#   every(1.day, 'shichimi', :at => '06:00') do
-    weather_forecast.each do |par|
-      client_rest.update(par)
-    end
-#   end
+weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
+
+include Clockwork
+every(1.day, 'shichimi', :at => '00:23') do
+  weather_forecast.each do |par|
+    client_rest.update(par)
+  end
+end
