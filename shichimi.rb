ef4dc78c741e3653095338511c37255455ca0d1b
Tomoharu Ito <treeoflife1103@gmail.com>
2017-12-28 00:55:19 +0900

    Development of SHICHIMI

--- shichimi.rb
+++ shichimi.rb
@@ -15,7 +15,13 @@ client_rest = Twitter::REST::Client.new(
   access_token_secret: ENV['ACCESS_TOKEN_SECRET']
 )
 
-# client_streaming = Twitter::Streaming::Client.new(keys)
+client_streaming = Twitter::Streaming::Client.new(
+  consumer_key: ENV['CONSUMER_KEY'],
+  consumer_secret: ENV['CONSUMER_SECRET'],
+  access_token: ENV['ACCESS_TOKEN'],
+  access_token_secret: ENV['ACCESS_TOKEN_SECRET']
+)
+
 uri_nagano = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200010')
 uri_matsumoto = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200020')
 uri_iida = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=200030')
@@ -29,7 +35,7 @@ weather = result['description']['text']
 weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
 
 include Clockwork
-every(1.day, 'shichimi', :at => '00:23') do
+every(1.day, 'shichimi', :at => '06:00') do
   weather_forecast.each do |par|
     client_rest.update(par)
   end
