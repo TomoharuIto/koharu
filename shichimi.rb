0707b2973f2ce63345e4e5c473d8e08241586888
Tomoharu Ito <treeoflife1103@gmail.com>
2017-12-29 20:44:02 +0900

    Development of SHICHIMI

--- shichimi.rb
+++ shichimi.rb
@@ -40,28 +40,35 @@ north = weather_info.call(200010)
 central = weather_info.call(200020)
 south = weather_info.call(200030)
 region = north['pinpointLocations']|central['pinpointLocations']|south['pinpointLocations']
-
-# location = /松本/
-# location_match = region.select{|area| area['name'] =~ location}
+location = /松本/
+location_match = region.select{|area| area['name'] =~ location}
 # location_match[0].each_value{|val| pp val}
 
 client_streaming.user do |status|
   case status
   when Twitter::DirectMessage
-      puts status.full_text
+    status.full_text.scan(%r|\s?[#＃]\s?(.{1,3})\s?|)
+    name = status.sender.screen_name
+    if(region.select{|area| area['name'] =~ /$1/})
+      place = location_match[0]["name"]
+      link = location_match[0]["link"]
+      client_rest.create_direct_message(status.sender.id, "#{name}さん、#{place}の天気へのリンクは#{link}です。")
+    elsif(status.full_text =~ /ping/)
+      client_rest.create_direct_message(status.sender.id, "PONG")
     end
   end
-
-public_time = central['description']['publicTime']
-date_time = DateTime.parse(public_time)
-suffix = %w(お を の もふ よ ぽ と)
-announcement_time = date_time.strftime("%m月%d日 %H時%M分 発表の予報です#{suffix.sample}。\n\n")
-weather = central['description']['text']
-weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
-
-include Clockwork
-every(1.day, 'shichimi', :at => '06:00') do
-  weather_forecast.each do |par|
-    client_rest.update(par)
-  end
 end
+
+# public_time = central['description']['publicTime']
+# date_time = DateTime.parse(public_time)
+# suffix = %w(お を の もふ よ ぽ と)
+# announcement_time = date_time.strftime("%m月%d日 %H時%M分 発表の予報です#{suffix.sample}。\n\n")
+# weather = central['description']['text']
+# weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
+#
+# include Clockwork
+# every(1.day, 'shichimi', :at => '06:00') do
+#   weather_forecast.each do |par|
+#     client_rest.update(par)
+#   end
+# end
