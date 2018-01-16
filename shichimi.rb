c1e37d468b0734f8cc2971b48ed7f23940a112b6
Tomoharu Ito <treeoflife1103@gmail.com>
2018-01-17 00:01:07 +0900

    Remove 'clockwork' from gem list

--- shichimi.rb
+++ shichimi.rb
@@ -1,4 +1,4 @@
-require 'clockwork'
+# require 'clockwork'
 require 'date'
 require 'docomoru'
 require 'dotenv'
@@ -78,16 +78,16 @@ client_streaming.user do |status|
   end
 end
 
-public_time = central['description']['publicTime']
-date_time = DateTime.parse(public_time)
-suffix = %w(お を の もふ よ ぽ と)
-announcement_time = date_time.strftime("%m月%d日 %H時%M分 発表の予報です#{suffix.sample}。\n\n")
-weather = central['description']['text']
-weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
-
-include Clockwork
-every(1.day, 'shichimi', :at => '06:00') do
-  weather_forecast.each do |par|
-    client_rest.update(par)
-  end
-end
+# public_time = central['description']['publicTime']
+# date_time = DateTime.parse(public_time)
+# suffix = %w(お を の もふ よ ぽ と)
+# announcement_time = date_time.strftime("%m月%d日 %H時%M分 発表の予報です#{suffix.sample}。\n\n")
+# weather = central['description']['text']
+# weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
+#
+# include Clockwork
+# every(1.day, 'shichimi', :at => '06:00') do
+#   weather_forecast.each do |par|
+#     client_rest.update(par)
+#   end
+# end
