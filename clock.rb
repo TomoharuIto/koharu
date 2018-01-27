cbdc74e6808c43bff2cfe23b83cde78cca051a2d
Tomoharu Ito <treeoflife1103@gmail.com>
2018-01-27 16:18:45 +0900

    Change emoji to colored

--- clock.rb
+++ clock.rb
@@ -44,16 +44,17 @@ module Weather
     telop = weather[i]['telop']
     image = weather[i]['image']['url']
     num = image.delete("^0-9")
-    emoji = num.gsub(num, "1" => "\u{2600}", "2" => "\u{2600}\/\u{2601}", "3" => "\u{2600}\/\u{2602}",
-    "4" => "\u{2600}\/\u{26C4}", "5" => "\u{2600}=>\u{2601}", "6" => "\u{2600}=>\u{2602}",
-    "7" => "\u{2600}=>\u{26C4}", "8" => "\u{2601}", "9" => "\u{2601}\/\u{2600}",
-    "10" => "\u{2601}\/\u{2602}", "11" => "\u{2601}\/\u{26C4}", "12" => "\u{2601}=>\u{2600}",
-    "13" => "\u{2601}=>\u{26C4}", "14" => "\u{2601}=>\u{26C4}", "15" => "\u{2602}",
-    "16" => "\u{2602}\/\u{2600}", "17" => "\u{2602}\/\u{2601}", "18" => "\u{2602}\/\u{26C4}",
-    "19" => "\u{2602}=>\u{2600}", "20" => "\u{2602}=>\u{2601}", "21" => "\u{2602}=>\u{26C4}",
-    "22" => "\u{2614}", "23" => "\u{26C4}", "24" => "\u{26C4}\/\u{2600}", "25" => "\u{26C4}\/\u{2601}",
-    "26" => "\u{26C4}\/\u{2602}", "27" => "\u{26C4}=>\u{2600}", "28" => "\u{26C4}=>\u{2601}",
-    "29" => "\u{26C4}=>\u{2602}", "30" => "\u{2603}")
+    emoji = num.gsub(num, "1" => "\u{2600 FE0F}", "2" => "\u{2600 FE0F}\/\u{2601 FE0F}", "3" => "\u{2600 FE0F}\/\u{2602 FE0F}",
+    "4" => "\u{2600 FE0F}\/\u{26C4 FE0F}", "5" => "\u{2600 FE0F}→\u{2601 FE0F}", "6" => "\u{2600 FE0F}→\u{2602 FE0F}",
+    "7" => "\u{2600 FE0F}→\u{26C4 FE0F}", "8" => "\u{2601 FE0F}", "9" => "\u{2601 FE0F}\/\u{2600 FE0F}",
+    "10" => "\u{2601 FE0F}\/\u{2602 FE0F}", "11" => "\u{2601 FE0F}\/\u{26C4 FE0F}", "12" => "\u{2601 FE0F}→\u{2600 FE0F}",
+    "13" => "\u{2601 FE0F}→\u{26C4 FE0F}", "14" => "\u{2601 FE0F}→\u{26C4 FE0F}", "15" => "\u{2602 FE0F}",
+    "16" => "\u{2602 FE0F}\/\u{2600 FE0F}", "17" => "\u{2602 FE0F}\/\u{2601 FE0F}", "18" => "\u{2602 FE0F}\/\u{26C4 FE0F}",
+    "19" => "\u{2602 FE0F}→\u{2600 FE0F}", "20" => "\u{2602 FE0F}→\u{2601 FE0F}", "21" => "\u{2602 FE0F}→\u{26C4 FE0F}",
+    "22" => "\u{2614 FE0F}", "23" => "\u{26C4 FE0F}", "24" => "\u{26C4 FE0F}\/\u{2600 FE0F}", "25" => "\u{26C4 FE0F}\/\u{2601 FE0F}",
+    "26" => "\u{26C4 FE0F}\/\u{2602 FE0F}", "27" => "\u{26C4 FE0F}→\u{2600 FE0F}", "28" => "\u{26C4 FE0F}→\u{2601 FE0F}",
+    "29" => "\u{26C4 FE0F}→\u{2602 FE0F}", "30" => "\u{2603 FE0F}")
+    pp emoji
     temperature_max = weather[i]['temperature']['max']
     unless temperature_max == nil
       temperature_max = temperature_max['celsius']
@@ -74,24 +75,22 @@ north_weather = "北部\n" + forecast(north, 0)<<forecast(north, 1)
 central_weather ="中部\n" +  forecast(central, 0)<<forecast(central, 1)
 south_weather = "南部\n" + forecast(south, 0)<<forecast(south, 1)
 region_weather = ["#{north_weather}", "#{central_weather}", "#{south_weather}"].reverse
-
 region = north['pinpointLocations']|central['pinpointLocations']|south['pinpointLocations']
 public_time = central['description']['publicTime']
 date_time = DateTime.parse(public_time)
 suffix = %w(お を の もふ よ ぽ と)
 announcement_time = date_time.strftime("%m月%d日 %H時%M分 発表の予報です#{suffix.sample}。\n\n")
 weather = central['description']['text'].gsub(/\s|。/,"/s" => "", "。" => "。\n\n")
-weather_forecast = (announcement_time << weather).scan(/.{1,139}。/m).reverse
-
-include Clockwork
-every(1.day, 'noon', :at => '12:00') do
-  weather_forecast.each do |par|
-    client_rest.update(par)
-  end
-end
-
-every(1.day, 'evening', :at => '18:00') do
-  region_weather.each do  |par|
-    client_rest.update(par)
-  end
- end
+weather_forecast = (announcement_time << weather).scan(/.{1,139}\n/m).reverse
+# include Clockwork
+# every(1.day, 'noon', :at => '12:00') do
+#   weather_forecast.each do |par|
+#     client_rest.update(par)
+#   end
+# end
+#
+# every(1.day, 'evening', :at => '18:00') do
+#   region_weather.each do  |par|
+#     client_rest.update(par)
+#   end
+#  end
